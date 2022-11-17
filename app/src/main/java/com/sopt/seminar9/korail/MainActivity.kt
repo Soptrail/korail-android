1ff8a7d5b2957e04cc0f4c825a311f8ff9225e7b
sujin-kk <sjoo0002@gmail.com>
2022-11-18 01:12:38 +0900

    [create] 프로젝트 생성

---
+++ app/src/main/java/com/sopt/seminar9/korail/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sopt.seminar9.korail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
