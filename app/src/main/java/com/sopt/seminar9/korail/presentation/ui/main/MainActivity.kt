a2b7dd681cbd5acdd36d88ac7db7cb8945898862
Chae's Mac Pro <chaeseung-un@Chaesui-MacBookPro.local>
2022-11-23 21:53:56 +0900

    [fix] 테스트용 코드 정리

--- app/src/main/java/com/sopt/seminar9/korail/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/seminar9/korail/presentation/ui/main/MainActivity.kt
@@ -32,8 +32,8 @@ class MainActivity : BindingActivity<ActivityMainBinding>({ ActivityMainBinding.
 
     private fun changeFragment(menuItemId: Int) = when (menuItemId) {
         R.id.menu_book_ticket -> replace<BookTicketFragment>(R.id.fcv_main, BookTicketFragment::class.java.simpleName)
-        R.id.menu_discount_ticket -> replace<ShowTrainFragment>(R.id.fcv_main) // 열차 조회 화면 테스트용 코드
-        // R.id.menu_discount_ticket -> replace<DiscountTicketFragment>(R.id.fcv_main)
+        // R.id.menu_discount_ticket -> replace<ShowTrainFragment>(R.id.fcv_main) // 열차 조회 화면 테스트용 코드
+        R.id.menu_discount_ticket -> replace<DiscountTicketFragment>(R.id.fcv_main)
         R.id.menu_tour_product -> replace<TourProductFragment>(R.id.fcv_main)
         R.id.menu_check_ticket -> replace<CheckTicketFragment>(R.id.fcv_main)
         else -> Timber.e(IllegalArgumentException(getString(R.string.error_not_found_menu_item)))
